c1de3af8207baafb982b5cf9e6b9e37155b46bc0
Rodrigo Montiel <montielrodrigo0@gmail.com>
2022-08-14 18:24:41 -0300

    Update Mapa de Anfibios.R

--- codigo/Scripts_de_Prueba/Mapa de Anfibios.R
+++ codigo/Scripts_de_Prueba/Mapa de Anfibios.R
@@ -10,13 +10,16 @@ NatUY <- read_csv("datos/observations-248320.csv")
 
 Uruguay <- load_geouy("Deptos", folder = "datos")
 
-# Filtrado de: Todos los anfibios en Uruguay + 
+# Filtrado de NatUY: Todos los anfibios + 
 # observados en octubre de 2021 + 
 # con especie confirmada (Grado de investigación)
 
 Anfibios <- NatUY %>% 
   filter(taxon_class_name == "Amphibia" & quality_grade == "research" & 
          between(observed_on, as.Date("2021-10-01"), as.Date("2021-10-31")))
+
+# Transformamos la tabla de Anfibios (spec_tbl_df) en un sf (Simple Features)
+# Como lo es Uruguay
 
 Anfibios_XY <- Anfibios %>% 
   select(scientific_name, observed_on, latitude, longitude) %>% 
@@ -26,16 +29,20 @@ Anfibios_XY <- Anfibios %>%
          
 
 
-#Poligono de Uruguay + puntos de anfibios
+# Poligono de Uruguay + puntos de anfibios
 Uruguay_Anfibios <- st_join(Uruguay, Anfibios_XY)
 
 
 Uruguay_Anfibios <- Uruguay_Anfibios %>%
-  group_by(gid) %>% 
+  group_by(nombre) %>% 
   summarise(riqueza = n_distinct(scientific_name), 
             abundancia = n())
 
 
-#Mapa de los anfibios filtrados
+#Mapa de riqueza de los anfibios filtrados
 ggplot(data = Uruguay_Anfibios) + 
   geom_sf(aes(fill=riqueza)) + theme_bw()
+
+#Mapa de abundancia de los anfibios filtrados
+ggplot(data = Uruguay_Anfibios) + 
+  geom_sf(aes(fill=abundancia)) + theme_bw()
