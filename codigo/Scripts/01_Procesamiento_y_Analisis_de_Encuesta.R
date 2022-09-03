ec348d920a607b2dcb31230bd8e7b3a284b60243
Rodrigo Montiel <montielrodrigo0@gmail.com>
2022-09-02 22:45:33 -0300

    Actualizacion de scripts de Prueba y nuevos scripts definitivos

--- codigo/Scripts/01_Procesamiento_y_Analisis_de_Encuesta.R
+++ codigo/Scripts/01_Procesamiento_y_Analisis_de_Encuesta.R
@@ -1,26 +1,30 @@
-# Paquetes y datos
+# PAQUETES Y DATOS ----------------------------------------------------------
 library(RColorBrewer)
 library(sf)
 library(tidyverse)
 Encuesta <- read_csv("datos/Encuesta.csv")
 
- 
-# Para ver las clases de cada columna
+
+# LIMPIEZA DE DATOS ---------------------------------------------------------
+
+# Si se quiere comprobar las clases de cada columna
 lapply(Encuesta,class)
 str(Encuesta)
 
- 
-# Cambiar Edad y Frecuencia de Caracter a numérico
+
+# Cambiar la clase de "Edad" y "Frecuencia" (caracter a numérico)
 Encuesta$Edad <- as.numeric(Encuesta$Edad)
 Encuesta$Frecuencia <- as.numeric(Encuesta$Frecuencia)
 
- ## Eliminar los NA
- Encuesta <- na.omit(Encuesta)
- 
- 
+   ## Eliminar los NA
+   Encuesta <- na.omit(Encuesta)
+
+
 # Estadisticas
 summary(Encuesta)
 
+
+# GRAFICOS ------------------------------------------------------------------
 
 # Histograma de edades
 ggplot(Encuesta) +
@@ -38,16 +42,16 @@ ggplot(Encuesta) +
   facet_grid(~Departamento) +
   theme_gray()
 
-  ## Afinidad por la naturaleza en función de la edad
-  ggplot(Encuesta) + 
-    geom_boxplot(aes(x=Afinidad_Naturaleza, y=Edad, fill=Departamento),
-                 show.legend = F) +
-    scale_fill_brewer(palette = "Spectral") +
-    facet_wrap(~Departamento) +
-    xlim(1,5) +
-    labs(y='Edad', x='Afinidad por la Naturaleza') +
-    theme_gray()
-    
+   ## Afinidad por la naturaleza en función de la edad
+   ggplot(Encuesta) + 
+     geom_boxplot(aes(x=Afinidad_Naturaleza, y=Edad, fill=Departamento),
+                  show.legend = F) +
+     scale_fill_brewer(palette = "Spectral") +
+     facet_wrap(~Departamento) +
+     xlim(1,5) +
+     labs(y='Edad', x='Afinidad por la Naturaleza') +
+     theme_gray()
+
 
 # Afinidad por la tecnologia en cada departamento
 ggplot(Encuesta) +
@@ -59,15 +63,14 @@ ggplot(Encuesta) +
   facet_grid(~Departamento) +
   theme_gray()
 
-  ## Afinidad por la tecnologia en función de la edad
-  ggplot(Encuesta) + 
-    geom_boxplot(aes(x=Afinidad_Tecnologia, y=Edad, fill=Departamento),
-               show.legend = F) +
-    scale_fill_brewer(palette = "Spectral") +
-    facet_wrap(~Departamento) +
-    xlim(1,3) +
-    labs(y='Edad', x='Afinidad por la Tecnologia') +
-    theme_gray()
+   ## Afinidad por la tecnologia en función de la edad
+   ggplot(Encuesta) + 
+     geom_boxplot(aes(x=Afinidad_Tecnologia, y=Edad, fill=Departamento),
+                  show.legend = F) +
+     scale_fill_brewer(palette = "Spectral") +
+     facet_wrap(~Departamento) +
+     labs(y='Edad', x='Afinidad por la Tecnologia') +
+     theme_gray()
 
 
 # Frecuencia de salida a la Naturaleza 
@@ -79,15 +82,11 @@ ggplot(Encuesta) +
   facet_wrap(~Departamento) +
   theme_gray()
 
-## Frecuencia de salidas en funcion de la edad
-ggplot(Encuesta) + 
-  geom_boxplot(aes(x=Frecuencia, y=Edad, fill=Departamento),
-               show.legend = F) +
-  scale_fill_brewer(palette = "Greens") +
-  facet_wrap(~Departamento) +
-  labs(y='Edad', x='Frecuencia de salidas') +
-  theme_gray()
-
-
-
-
+   ## Frecuencia de salidas en funcion de la edad
+   ggplot(Encuesta) + 
+     geom_boxplot(aes(x=Frecuencia, y=Edad, fill=Departamento),
+                  show.legend = F) +
+     scale_fill_brewer(palette = "Greens") +
+     facet_wrap(~Departamento) +
+     labs(y='Edad', x='Frecuencia de salidas') +
+     theme_gray()
