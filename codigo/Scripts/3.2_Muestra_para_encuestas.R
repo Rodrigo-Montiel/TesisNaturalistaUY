b9fb0e545dccc77fb0cc03d237d4bf6af77fa037
Rodrigo Montiel <montielrodrigo0@gmail.com>
2022-11-12 18:27:34 -0300

    Update Scripts definitivos

--- codigo/Scripts/3.2_Muestra_para_encuestas.R
+++ codigo/Scripts/3.2_Muestra_para_encuestas.R
@@ -1,4 +1,20 @@
-### Sample de usuarios para las encuestas
+# PAQUETES Y DATOS--------------------------------------------------------------
+
+usuarios_dataset <- readRDS("datos/usuarios_dataset.rds")
+
+# MUESTRAS----------------------------------------------------------------------
+
+##Categoria: Principiantes
 usuarios_dataset %>% filter(categoria_usuario=="principiante" & 
                               ultimo_registro<(today()-365)) %>% 
   pull(user_login) %>% sample(., size=100)
+
+##Categoria: Intermedios
+usuarios_dataset %>% filter(categoria_usuario=="intermedio" & 
+                              ultimo_registro<(today()-365)) %>% 
+  pull(user_login) %>% sample(., size=20)
+
+##Categoria: Experimentados
+usuarios_dataset %>% filter(categoria_usuario=="experimentados" & 
+                              ultimo_registro<(today()-365)) %>% 
+  pull(user_login) %>% sample(., size=5)
