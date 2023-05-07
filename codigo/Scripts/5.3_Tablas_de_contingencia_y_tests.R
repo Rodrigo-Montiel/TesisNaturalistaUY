cc7b5e48cd579a7ef47e9841fad1705e4d9a6498
Rodrigo Montiel <montielrodrigo0@gmail.com>
2023-05-07 12:24:58 -0300

    Update 5.3_Tablas_de_contingencia_y_tests.R

--- codigo/Scripts/5.3_Tablas_de_contingencia_y_tests.R
+++ codigo/Scripts/5.3_Tablas_de_contingencia_y_tests.R
@@ -4,7 +4,7 @@ library(tidyverse)
 registros_de_tetrapodosuy <- read_csv('datos/registros_de_tetrapodos.uy.csv')
 
 
-# TABLAS DE CONTINGENCIA--------------------------------------------------------
+# CHI CUADRADO------------------------------------------------------------------
 
 ## TETRÁPODOS  
 
@@ -12,44 +12,67 @@ registros_de_tetrapodosuy <- read_csv('datos/registros_de_tetrapodos.uy.csv')
 exp_tam <- aggregate(nivel ~ tamaño, 
                      data = registros_de_tetrapodosuy, FUN = table)
 
-## Experticia vs distribución
-exp_dist <- aggregate(nivel ~ distribucion_2, 
-                      data = registros_de_tetrapodosuy, FUN = table)
+exp_tam <- exp_tam[,-1]
+rownames(exp_tam) <- exp_tam[,1]
+rownames(exp_tam) <- c('Grande', 'Mediano', 'chico')
 
-## Experticia vs estado de conservacion
-exp_stat <- aggregate(nivel ~ factor(status_global), 
-                      data = registros_de_tetrapodosuy, FUN = table)
-
-## PLANTAS
-
-##Experticia vs estado de conservacion
-exp_stat_p <- aggregate(nivel ~ factor(status_global), 
-                         data = registros_de_plantasuy, FUN = table)
-
-exp_hab1_p <- aggregate(nivel ~ Habito1,
-                        data = registros_de_plantasuy, FUN = table)
-
-# TEST CHI-CUADRADO-------------------------------------------------------------
-
-## TETRÁPODOS  
-
-### Experticia vs tamaño
-
-exp_tam2 <- exp_tam[,-1]
-rownames(exp_tam2) <- exp_tam[,1]
-rownames(exp_tam2) <- c('Grande', 'Mediano', 'chico')
-exp_tam2
-
-chi_exp_tam <- chisq.test(exp_tam2)
+chi_exp_tam <- chisq.test(exp_tam)
 chi_exp_tam
 
 
+### Experticia vs distribución
+exp_dist <- aggregate(nivel ~ distribucion_2, 
+                      data = registros_de_tetrapodosuy, FUN = table)
+
+exp_dist <- exp_dist[,-1]
+rownames(exp_dist) <- exp_dist[,1]
+rownames(exp_dist) <- c('Alta', 'Baja', 'Media')
+
+chi_exp_dist <- chisq.test(exp_dist)
+chi_exp_dist
+
+
+### Experticia vs estado de conservacion (ERROR)
+exp_stat <- aggregate(nivel ~ factor(status_global), 
+                      data = registros_de_tetrapodosuy, FUN = table)
+
+exp_stat <- exp_stat[,-1]
+rownames(exp_stat) <- exp_stat[,1]
+rownames(exp_stat) <- c('CR', 'DD', 'EN',"LC","NE","NT","VU")
+
+chi_exp_stat <- chisq.test(exp_stat)
+
+
 ## PLANTAS
 
-### Experticia vs Habito1
+### Experticia vs estado de conservacion (ERROR)
+exp_stat_p <- aggregate(nivel ~ factor(status_global), 
+                         data = registros_de_plantasuy, FUN = table)
+
+exp_stat_p <- exp_stat_p[,-1]
+rownames(exp_stat_p) <- exp_stat_p[,1]
+rownames(exp_stat_p) <- c('CR', 'DD', 'EN',"EW","LC","NE","NT","VU")
+
+chi_exp_stat <- chisq.test(exp_stat_P)
+
+
+### Experticia vs habito
+exp_hab1_p <- aggregate(nivel ~ Habito1,
+                        data = registros_de_plantasuy, FUN = table)
+
 exp_hab1_p <- exp_hab1_p[,-1]
 rownames(exp_hab1_p) <- exp_hab1_p[,1]
 rownames(exp_hab1_p) <- c("Arbol", "Arbusto", "Enredadera", "Hierba", "Liana",
                           "NE","Subarbusto")
 chi_exp_hab1_p <- chisq.test(exp_hab1_p)
 chi_exp_hab1_p
+
+
+# REGRESIONES-------------------------------------------------------------------
+
+## TETRAPODOS
+
+m_distribucion <- 
+  lm(ranking ~ distribucion, data = registros_de_tetrapodosuy)
+
+summary(Modelo_distribucion)
