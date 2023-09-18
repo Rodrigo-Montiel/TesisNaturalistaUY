796a803946a92234e44e613519590a96db9bc293
Rodrigo Montiel <montielrodrigo0@gmail.com>
2023-09-18 18:11:41 -0300

    Update 1_Contribuciones_NatUY.R

--- codigo/Scripts Finales/1_Contribuciones_NatUY.R
+++ codigo/Scripts Finales/1_Contribuciones_NatUY.R
@@ -2,12 +2,13 @@
 library(tidyverse)
 library(sf)
 sf::sf_use_s2(FALSE)
+library(dplyr)
 
 NatUY <- read_csv('datos/Observaciones_27-10-22.csv')
 
 # FILTRADO DE ESPECIES CASUALES-------------------------------------------------
-##De la categorización de registros (4_categorizacion_de_registros.R) nos 
-##encontramos con especies "casuales". Vamos a eliminarlas:
+## De la categorización de registros (4_categorizacion_de_registros.R) nos 
+## encontramos con especies "casuales". Vamos a eliminarlas:
 
 NatUY <- NatUY %>% filter(scientific_name!= "Anas platyrhynchos" | 
                             scientific_name!= "Anser anser" |
@@ -39,38 +40,11 @@ NatUY %>% st_drop_geometry() %>%
   count() %>% arrange(desc(n)) %>% nrow()
 
 
-## Registros con Grado de investigacion
+## Registros con Grado de investigación
 NatUY %>% st_drop_geometry() %>% filter(quality_grade == "research") %>% 
   group_by(quality_grade) %>% nrow()
-
-### Especies GI
-NatUY %>% st_drop_geometry() %>%  
-  group_by(scientific_name) %>% 
-  filter(quality_grade == "research") %>% 
-  filter(!is.na(scientific_name)) %>% 
-  filter(str_count(scientific_name, "\\S+") ==2 ) %>% 
-  count() %>% arrange(desc(n)) %>% nrow()
-
-
-## Registros que necesitan identificación
-
-NatUY %>% st_drop_geometry() %>% filter(quality_grade == "needs_id") %>% 
-  group_by(quality_grade) %>% nrow()
-
-### Especies ID
-NatUY %>% st_drop_geometry() %>%  
-  group_by(scientific_name) %>% 
-  filter(quality_grade == "needs_id") %>% 
-  filter(!is.na(scientific_name)) %>% 
-  filter(str_count(scientific_name, "\\S+") ==2 ) %>% 
-  count() %>% arrange(desc(n)) %>% nrow()
 
 
 ## Cantidad de usuarios
 NatUY %>% st_drop_geometry() %>% group_by(user_id) %>% 
   count() %>% arrange(desc(n)) %>% nrow()
-
-
-
-
-
