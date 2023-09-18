e676d5ef323cfd00137fcbfd18446265e8f8980c
Rodrigo Montiel <montielrodrigo0@gmail.com>
2023-09-17 23:26:45 -0300

    Update Cobertura teporal (facet wrap).R

--- codigo/Scripts_de_Prueba/Cobertura teporal (facet wrap).R
+++ codigo/Scripts_de_Prueba/Cobertura teporal (facet wrap).R
@@ -21,11 +21,11 @@ listado_especies %>% st_drop_geometry() %>%
   filter(!is.na(taxon_phylum_name)) %>% 
   filter(taxon_kingdom_name=='Animalia') %>% 
   group_by(year(observed_on), taxon_kingdom_name, taxon_phylum_name,
-           taxon_class_name) %>% count() %>% 
+           taxon_class_name) %>% count() %>%
   ggplot(aes(x=`year(observed_on)`, y= n, color=taxon_phylum_name,)) + 
   geom_line(size=1, show.legend = TRUE) + geom_point() +
   scale_x_continuous(breaks= scales::pretty_breaks()) +
-  facet_wrap(taxon_class_name~.,scales = "free",drop=TRUE) +
+  facet_wrap(taxon_class_name~.,scales = "free",drop=TRUE, nrow = 3) +
   labs(x='Años', y='Registros', color = 'filo') +
   theme_bw()
 
