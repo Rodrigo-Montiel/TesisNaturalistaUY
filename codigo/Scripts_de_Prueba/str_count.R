8b750e4946f99f5ebf1cea76594853b55ab0fdfc
Rodrigo Montiel <montielrodrigo0@gmail.com>
2023-09-20 17:10:35 -0300

    Update str_count.R

--- codigo/Scripts_de_Prueba/str_count.R
+++ codigo/Scripts_de_Prueba/str_count.R
@@ -1,22 +1,27 @@
-#CONTRIBUCIONES NATUY - CANTIDAD DE ESPECIES------------------------------------
+# ESTE SCRIPT ES PARA VER LAS DIFERENCIAS ENTRE USAR LA FUNCION STR_COUNT Y NO, 
+# PARA SELECCIONAR AQUELLOS NOMBRES CIENTIFICOS CON DOS PALAGRAS
 
-## Cantidad de Especies registradas
+#CONTRIBUCIONES NATUY - CANTIDAD DE REGISTROS Y ESPECIES------------------------
+
+## Cantidad de  registradas sin str_count
 Especies1 <- NatUY %>% st_drop_geometry() %>%
-  group_by(scientific_name) %>% 
-  filter(!is.na(scientific_name)) %>% 
-  filter(str_count(scientific_name, "\\S+") ==2 ) %>% 
-  count(n_distinct(scientific_name)) %>% arrange(desc(n)) %>% nrow()
-
-
-#### Cantidad de Especies registradas sin str_count
-Especies2 <- NatUY %>% st_drop_geometry() %>%
   group_by(scientific_name) %>% 
   filter(!is.na(scientific_name)) %>% 
   filter(scientific_name %in% unique(scientific_name)) %>% 
   count(n_distinct(scientific_name)) %>% arrange(desc(n)) %>% nrow()
 
 
+#### Cantidad de Especies registradas con str_count
+Especies2 <- NatUY %>% st_drop_geometry() %>%
+  group_by(scientific_name) %>% 
+  filter(!is.na(scientific_name)) %>% 
+  filter(str_count(scientific_name, "\\S+") ==2 ) %>% 
+  count(n_distinct(scientific_name)) %>% arrange(desc(n)) %>% nrow()
+
+
+
 # LISTADO DE ESPECIES-----------------------------------------------------------
+
 ## Filtrado de registros: GI + Nivel Especie
 listado_especies <- NatUY %>% 
   select(observed_on,quality_grade, place_admin1_name, 
@@ -26,7 +31,7 @@ listado_especies <- NatUY %>%
   filter(quality_grade == "research" & !is.na(taxon_species_name) & 
            taxon_species_name!="")
 
-## Filtrado de registros: GI + Nivel Especie
+## Filtrado de registros: GI + Nivel Especie con str_count
 listado_especies2 <- NatUY %>% 
   select(observed_on,quality_grade, place_admin1_name, 
          taxon_species_name,taxon_kingdom_name, taxon_phylum_name, 
